2c1b3d60f2efddaa32e41894b1b75318f198f261
Federico Catala Ortmann <fortmann@atixlabs.com>
2022-10-26 17:53:09 -0300

    fix: fix formatUserRoles tests

--- src/tests/services/helpers/formatUserRoles.test.js
+++ src/tests/services/helpers/formatUserRoles.test.js
@@ -27,38 +27,34 @@ describe('Testing formatUserRoles helper', () => {
   };
   describe('Test formatUserRoles method', () => {
     it('should return the same user properties with roles formated', () => {
-      expect(formatUserRoles([user])).toMatchObject([
-        {
-          id: 3,
-          firstName: 'Pablo',
-          lastName: 'Perez',
-          email: 'admin@test.com',
-          emailConfirmation: true,
-          roles: [
-            {
-              projectId: 1,
-              roles: [1, 2]
-            },
-            {
-              projectId: 2,
-              roles: [2]
-            }
-          ]
-        }
-      ]);
+      expect(formatUserRoles(user)).toMatchObject({
+        id: 3,
+        firstName: 'Pablo',
+        lastName: 'Perez',
+        email: 'admin@test.com',
+        emailConfirmation: true,
+        roles: [
+          {
+            projectId: 1,
+            roles: [1, 2]
+          },
+          {
+            projectId: 2,
+            roles: [2]
+          }
+        ]
+      });
     });
 
     it('should return the same user properties because roles field is empty array', () => {
-      expect(formatUserRoles([{ ...user, roles: [] }])).toMatchObject([
-        {
-          id: 3,
-          firstName: 'Pablo',
-          lastName: 'Perez',
-          email: 'admin@test.com',
-          emailConfirmation: true,
-          roles: []
-        }
-      ]);
+      expect(formatUserRoles({ ...user, roles: [] })).toMatchObject({
+        id: 3,
+        firstName: 'Pablo',
+        lastName: 'Perez',
+        email: 'admin@test.com',
+        emailConfirmation: true,
+        roles: []
+      });
     });
   });
 });
