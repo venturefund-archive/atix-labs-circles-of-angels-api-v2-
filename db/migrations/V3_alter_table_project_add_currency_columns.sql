80d01105c2ea672841a0fd84dcfafd7bd05765d8
Federico Catala Ortmann <fortmann@atixlabs.com>
2022-10-27 16:36:15 -0300

    fix: fix prefix name of migrations

--- db/migrations/V3_alter_table_project_add_currency_columns.sql
+++
@@ -1,6 +0,0 @@
-CREATE TYPE public.currency_type AS ENUM (
-    'fiat',
-    'crypto'
-);
-ALTER TABLE public.project ADD COLUMN "currencyType" public.currency_type;
-ALTER TABLE public.project ADD COLUMN "currency" character varying(50);
