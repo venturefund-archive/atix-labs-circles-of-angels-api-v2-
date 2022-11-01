399c3e0cd5c6a4bbf44bdd69e6ddf75ca4437731
Federico Catala <federico.catala@globant.com>
2022-10-31 21:57:48 -0300

    refactor: delete flyway table

--- db/scripts/schema.sql
+++ db/scripts/schema.sql
@@ -709,19 +709,6 @@ CREATE TABLE public.vote (
     status public.tx_proposal_status DEFAULT 'notsent'::public.tx_proposal_status
 );
 
-CREATE TABLE public.flyway_schema_history (
-    installed_rank integer NOT NULL,
-    version character varying(50),
-    description character varying(200) NOT NULL,
-    type character varying(20) NOT NULL,
-    script character varying(1000) NOT NULL,
-    checksum integer,
-    installed_by character varying(100) NOT NULL,
-    installed_on timestamp without time zone DEFAULT now() NOT NULL,
-    execution_time integer NOT NULL,
-    success boolean NOT NULL
-);
-
 CREATE SEQUENCE public.vote_id_seq
     AS integer
     START WITH 1
@@ -794,7 +781,3 @@ ALTER TABLE ONLY public.user_social_entrepreneur ALTER COLUMN id SET DEFAULT nex
 ALTER TABLE ONLY public.user_wallet ALTER COLUMN id SET DEFAULT nextval('public.user_wallet_id_seq'::regclass);
 
 ALTER TABLE ONLY public.vote ALTER COLUMN id SET DEFAULT nextval('public.vote_id_seq'::regclass);
-
-ALTER TABLE ONLY public.flyway_schema_history ADD CONSTRAINT flyway_schema_history_pk PRIMARY KEY (installed_rank);
-
-CREATE INDEX flyway_schema_history_s_idx ON public.flyway_schema_history USING btree (success);
