c97b480cb62357c41c76f14ebd7bc06806696fff
Federico Catala Ortmann <fortmann@atixlabs.com>
2022-11-18 16:48:34 -0300

    fix: fix migration V19 to add primary keys to task_evidence and file

--- db/migrations/V19__add_table_evidence_file.sql
+++ db/migrations/V19__add_table_evidence_file.sql
@@ -1,3 +1,7 @@
+ALTER TABLE ONLY public.task_evidence ADD CONSTRAINT task_evidence_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY public.file ADD CONSTRAINT file_pkey PRIMARY KEY (id);
+
 CREATE TABLE "evidence_file" (
     id SERIAL primary KEY,
     "taskEvidenceId" integer NOT NULL CONSTRAINT "evidence_file_taskEvidenceId_fkey" REFERENCES task_evidence(id),
