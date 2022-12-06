b4833f431a0847f5c7688096946a16736e60a08c
Federico Catala Ortmann <fortmann@atixlabs.com>
2022-12-06 15:16:35 -0300

    refactor: add migration to add new status to 'projectstatus' enum

--- db/migrations/V33__alter_project_status_enum.sql
+++ db/migrations/V33__alter_project_status_enum.sql
@@ -1,26 +1,4 @@
-ALTER TABLE public.project DROP COLUMN "status";
-
-DROP TYPE IF EXISTS ProjectStatus;
-CREATE TYPE ProjectStatus AS ENUM (
-  'draft',
-  'published',
-  'in progress',
-  'in review',
-  'completed',
-  'canceled',
-  'new',
-  'toreview',
-  'rejected',
-  'deleted',
-  'consensus',
-  'funding',
-  'executing',
-  'changingscope',
-  'finished',
-  'aborted',
-  'archived',
-  'cancelled'
-);
-
-ALTER TABLE public.project ALTER COLUMN status TYPE ProjectStatus USING status::text::ProjectStatus;
-ALTER TABLE public.project ALTER COLUMN status SET DEFAULT 'draft';
+ALTER TYPE ProjectStatus ADD VALUE 'in progress';
+ALTER TYPE ProjectStatus ADD VALUE 'in review';
+ALTER TYPE ProjectStatus ADD VALUE 'completed';
+ALTER TYPE ProjectStatus ADD VALUE 'canceled';
