918eeaef31571e6d4c1e639cea42202d7ce9986f
Juan Cruz <jvieiro@atixlabs.com>
2022-12-26 14:32:41 -0300

    fix:  add validation for amount sign at addEvidence

---
+++ db/migrations/V47__rename_investor_role_per_funder.sql
@@ -0,0 +1,1 @@
+UPDATE public.role SET description = 'funder' WHERE description = 'investor';
--- src/rest/services/activityService.js
+++ src/rest/services/activityService.js
@@ -1062,7 +1062,6 @@ module.exports = {
     });
 
     this.validateStatusToUploadEvidence({ status: project.status });
-
     try {
       let savedFiles = [];
       if (
@@ -1086,14 +1085,12 @@ module.exports = {
       }
 
       const initIncomeOutcome = { income: '0', outcome: '0' };
-
       const assignedAmount =
         !amount || amount === '0'
           ? initIncomeOutcome
           : this.assignAmountToIncomeOrOutcome(
               currencyType === currencyTypes.CRYPTO ? cryptoAmount : amount
             );
-
       const evidence = {
         title,
         description,
@@ -1112,8 +1109,6 @@ module.exports = {
       const taskEvidences = await this.taskEvidenceDao.getEvidencesByTaskId(
         activity.id
       );
-      logger.info(taskEvidences.length === 0 ||
-        activity.status === ACTIVITY_STATUS.REJECTED);
       if (
         taskEvidences.length === 0 ||
         activity.status === ACTIVITY_STATUS.REJECTED
@@ -1236,9 +1231,13 @@ module.exports = {
   },
 
   assignAmountToIncomeOrOutcome(amount) {
+    const isAmountGreaterThanZero = BigNumber(amount).isGreaterThan(0);
     const income = { income: amount, outcome: '0' };
-    const outcome = { income: '0', outcome: removeMinusSign(amount) };
-    return BigNumber(amount).isGreaterThan(0) ? income : outcome;
+    const outcome = {
+      income: '0',
+      outcome: !isAmountGreaterThanZero ? removeMinusSign(amount) : amount
+    };
+    return isAmountGreaterThanZero ? income : outcome;
   },
 
   /**
