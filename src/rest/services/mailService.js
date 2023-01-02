34e3bd7c6363f963ff44b8b221bf42cdcbbfd8d7
Federico Catala Ortmann <fortmann@atixlabs.com>
2023-01-02 11:58:21 -0300

    refactor: changes in mail service to refactor sendEmails function

--- src/rest/services/mailService.js
+++ src/rest/services/mailService.js
@@ -18,6 +18,8 @@ const { templateNames } = require('../services/helpers/templateLoader');
 const logger = require('../logger');
 const languages = require('../../../projects/languages/default.json');
 const { ACTION_TYPE } = require('../util/constants');
+const emailClient = require('../services/helpers/emailClient');
+const projectService = require('../services/projectService');
 
 const FRONTEND_URL = config.frontendUrl;
 const IMAGES_URL = `${FRONTEND_URL}/static/images`;
@@ -26,299 +28,319 @@ const URL_LOCKED_WINDOW = `${IMAGES_URL}/locked-window.png`;
 const URL_UPLOAD_TO_CLOUD = `${IMAGES_URL}/upload-to-cloud.png`;
 const TEMPLATES_DIRECTORY_PATH = `${process.cwd()}/assets/templates/email`;
 
+const getHTMLFromMJML = ({ mjmlFileName, objectData }) => {
+  const mjmlFileContent = fs.readFileSync(
+    `${TEMPLATES_DIRECTORY_PATH}/${mjmlFileName}.mjml`
+  );
+  let htmlOutput = mjml2html(mjmlFileContent.toString()).html;
+  Object.keys(objectData).forEach(key => {
+    const regExp = new RegExp(`{{${key}}}`, 'g');
+    htmlOutput = htmlOutput.replace(regExp, objectData[key]);
+  });
+  return htmlOutput;
+};
+
+const sendMail = async ({
+  to,
+  from = config.email.from,
+  subject,
+  text,
+  html
+}) => {
+  logger.info(`[MailService] :: Sending email to: ${to} subject: ${subject}`);
+  validateRequiredParams({
+    method: 'sendMail',
+    params: { to, from, subject, html }
+  });
+  try {
+    const info = await emailClient.sendMail({
+      to,
+      from,
+      subject,
+      text,
+      html
+    });
+    // why isEmpty?
+    if (!isEmpty(info.rejected)) {
+      throw new Error('Invalid email account');
+    }
+    return info;
+  } catch (error) {
+    logger.error('[MailService] :: Email was not sent', error);
+    throw new COAError(errors.mail.EmailNotSent);
+  }
+};
+
+const sendSignUpMail = async ({
+  to,
+  subject = 'Welcome to Circles of Angels',
+  text,
+  bodyContent
+}) => {
+  logger.info('[MailService] :: Sending sign up mail to:', to);
+  validateRequiredParams({
+    method: 'sendSignUpMail',
+    params: { to, subject, bodyContent }
+  });
+
+  const html = await templateParser.completeTemplate(
+    {
+      ...bodyContent,
+      frontendUrl: config.frontendUrl
+    },
+    templateNames.SIGNUP
+  );
+  await sendMail({ to, subject, text, html });
+};
+
+const sendProjectStatusChangeMail = async ({
+  to,
+  subject = 'Circles of Angels: A project has been updated',
+  text,
+  bodyContent
+}) => {
+  logger.info('[MailService] :: Sending project status change mail to:', to);
+  validateRequiredParams({
+    method: 'sendProjectStatusChangeMail',
+    params: { to, subject, bodyContent }
+  });
+
+  const html = await templateParser.completeTemplate(
+    {
+      ...bodyContent,
+      frontendUrl: config.frontendUrl
+    },
+    templateNames.PROJECT_STATUS_CHANGE
+  );
+  await sendMail({ to, subject, text, html });
+};
+
+const sendEmailVerification = async ({
+  to,
+  subject = 'Circles of Angels: Account verification',
+  text,
+  bodyContent
+}) => {
+  logger.info('[MailService] :: Sending verification mail to:', to);
+  validateRequiredParams({
+    method: 'sendEmailVerification',
+    params: { to, subject, bodyContent }
+  });
+
+  const html = await templateParser.completeTemplate(
+    {
+      ...bodyContent,
+      frontendUrl: config.frontendUrl
+    },
+    templateNames.EMAIL_CONFIRMATION
+  );
+  await sendMail({ to, subject, text, html });
+};
+
+const sendEmailRecoveryPassword = async ({
+  to,
+  subject = 'Circles of Angels: Recovery Password',
+  text,
+  bodyContent
+}) => {
+  logger.info('[MailService] :: Sending recovery password mail to:', to);
+  validateRequiredParams({
+    method: 'sendEmailRecoveryPassword',
+    params: { to, subject, bodyContent }
+  });
+  const baseUrl = bodyContent.projectId
+    ? `${FRONTEND_URL}/${bodyContent.projectId}`
+    : `${FRONTEND_URL}/u`;
+  const html = getHTMLFromMJML({
+    mjmlFileName: templateNames.RECOVERY_PASSWORD,
+    objectData: {
+      ...bodyContent,
+      ...languages.resetPasswordEmail,
+      frontendUrl: baseUrl,
+      URL_LOGO,
+      URL_LOCKED_WINDOW
+    }
+  });
+  await sendMail({ to, subject, text, html });
+};
+
+const sendEmailInitialRecoveryPassword = async ({
+  to,
+  subject = 'Circles of Angels: Reset Password',
+  text,
+  bodyContent
+}) => {
+  logger.info('[MailService] :: Sending recovery password mail to:', to);
+  validateRequiredParams({
+    method: 'sendEmailRecoveryPassword',
+    params: { to, subject, bodyContent }
+  });
+
+  const html = getHTMLFromMJML({
+    mjmlFileName: templateNames.WELCOME,
+    objectData: {
+      ...bodyContent,
+      frontendUrl: FRONTEND_URL,
+      URL_LOGO,
+      URL_LOCKED_WINDOW,
+      ...languages.welcomeEmail
+    }
+  });
+  await sendMail({ to, subject, text, html });
+};
+
+const sendLowBalanceGSNAccountEmail = async (to, account, balance) => {
+  logger.info(
+    `[MailService] :: Sending low balance in Main account ${account} to ${to}`
+  );
+  validateRequiredParams({
+    method: 'sendLowBalanceGSNAccountEmail',
+    params: { to, account, balance }
+  });
+
+  const html = await templateParser.completeTemplate(
+    {
+      title: 'Alert COA main account is running out of balance.',
+      bodyText: `This is a reminder that the COA Gas station is running out of money, having currently ${balance} WEIs left. 
+Please add more to avoid transactions not being able to execute. 
+Remember the address to transfer the money to is: ${account}`
+    },
+    templateNames.ALERT
+  );
+  const subject = 'COA Gas Station Balance Alert';
+  await sendMail({ to, subject, html });
+};
+
+const sendInitialUserResetPassword = async ({
+  to,
+  subject = 'Circles of Angels: Reset Password',
+  text,
+  bodyContent
+}) => {
+  logger.info('[MailService] :: Sending recovery password mail to:', to);
+  validateRequiredParams({
+    method: 'sendInitialUserResetPassword',
+    params: { to, subject, bodyContent }
+  });
+  const baseUrl = bodyContent.projectId
+    ? `${FRONTEND_URL}/${bodyContent.projectId}`
+    : `${FRONTEND_URL}/u`;
+
+  const html = getHTMLFromMJML({
+    mjmlFileName: templateNames.WELCOME_USER,
+    objectData: {
+      ...bodyContent,
+      ...languages.welcomeUserEmail,
+      frontendUrl: baseUrl,
+      URL_LOGO,
+      URL_LOCKED_WINDOW
+    }
+  });
+  await sendMail({ to, subject, text, html });
+};
+
+const sendPublishProject = async ({
+  to,
+  subject = 'Circles of Angels: Project published',
+  text,
+  bodyContent
+}) => {
+  validateRequiredParams({
+    method: 'sendPublishProject',
+    params: { to, subject, bodyContent }
+  });
+  const html = getHTMLFromMJML({
+    mjmlFileName: templateNames.PUBLISH_PROJECT,
+    objectData: {
+      ...bodyContent,
+      ...languages.publishedProjectEmail,
+      frontendUrl: FRONTEND_URL,
+      URL_LOGO,
+      URL_UPLOAD_TO_CLOUD
+    }
+  });
+  await sendMail({ to, subject, text, html });
+};
+
+const sendEmailCloneInReview = async ({
+  to,
+  subject = 'Circles of Angels: Project review have been requested',
+  text,
+  bodyContent
+}) => {
+  validateRequiredParams({
+    method: 'sendEmailProjectInReview',
+    params: { to, subject, bodyContent }
+  });
+  const html = getHTMLFromMJML({
+    mjmlFileName: templateNames.CLONE_IN_REVIEW,
+    objectData: {
+      ...bodyContent,
+      ...languages.cloneInReviewEmail,
+      frontendUrl: FRONTEND_URL,
+      URL_LOGO,
+      URL_UPLOAD_TO_CLOUD
+    }
+  });
+  await sendMail({ to, subject, text, html });
+};
+
 const SEND_EMAIL_BY_ACTION = {
-  [ACTION_TYPE.PUBLISH_PROJECT]: 'sendPublishProject',
-  [ACTION_TYPE.SEND_PROJECT_TO_REVIEW]: 'sendEmailCloneInReview'
+  [ACTION_TYPE.PUBLISH_PROJECT]: sendPublishProject,
+  [ACTION_TYPE.SEND_PROJECT_TO_REVIEW]: sendEmailCloneInReview
+};
+
+const sendEmails = async ({ project, action, users }) => {
+  try {
+    logger.info('[ProjectService] :: About to send project action emails');
+    const sendEmail = SEND_EMAIL_BY_ACTION[action];
+    const usersToSendEmail =
+      users ||
+      (await projectService.getUsersByProjectId({
+        projectId: project.id
+      }));
+    const { projectName, id } = project;
+    const bodyContent = {
+      projectName,
+      projectId: id
+    };
+    await Promise.all(
+      usersToSendEmail.map(({ email }) =>
+        sendEmail({
+          to: email,
+          bodyContent
+        })
+      )
+    );
+  } catch (error) {
+    logger.error(
+      '[ProjectService] :: There was an error trying to send project action emails',
+      error
+    );
+    throw new COAError(errors.mail.EmailNotSent);
+  }
 };
 
 module.exports = {
-  /**
-   * Sends an email.
-   *
-   * @param {string} to - email's recipient.
-   * @param {string} from - email's sender.
-   * @param {string} subject - email's subject.
-   * @param {number} text <= TODO : what is this?
-   * @param {object} html - email's html content.
-   * @returns
-   */
-  async sendMail({ to, from = config.email.from, subject, text, html }) {
-    logger.info(`[MailService] :: Sending email to: ${to} subject: ${subject}`);
-    validateRequiredParams({
-      method: 'sendMail',
-      params: { to, from, subject, html }
-    });
-    try {
-      const info = await this.emailClient.sendMail({
-        to,
-        from,
-        subject,
-        text,
-        html
-      });
-      // why isEmpty?
-      if (!isEmpty(info.rejected)) {
-        throw new Error('Invalid email account');
-      }
-      return info;
-    } catch (error) {
-      logger.error('[MailService] :: Email was not sent', error);
-      throw new COAError(errors.mail.EmailNotSent);
-    }
-  },
+  sendMail,
 
-  async sendSignUpMail({
-    to,
-    subject = 'Welcome to Circles of Angels',
-    text,
-    bodyContent
-  }) {
-    logger.info('[MailService] :: Sending sign up mail to:', to);
-    validateRequiredParams({
-      method: 'sendSignUpMail',
-      params: { to, subject, bodyContent }
-    });
+  sendSignUpMail,
 
-    const html = await templateParser.completeTemplate(
-      {
-        ...bodyContent,
-        frontendUrl: config.frontendUrl
-      },
-      templateNames.SIGNUP
-    );
-    await this.sendMail({ to, subject, text, html });
-  },
+  sendProjectStatusChangeMail,
 
-  async sendProjectStatusChangeMail({
-    to,
-    subject = 'Circles of Angels: A project has been updated',
-    text,
-    bodyContent
-  }) {
-    logger.info('[MailService] :: Sending project status change mail to:', to);
-    validateRequiredParams({
-      method: 'sendProjectStatusChangeMail',
-      params: { to, subject, bodyContent }
-    });
+  sendEmailVerification,
 
-    const html = await templateParser.completeTemplate(
-      {
-        ...bodyContent,
-        frontendUrl: config.frontendUrl
-      },
-      templateNames.PROJECT_STATUS_CHANGE
-    );
-    await this.sendMail({ to, subject, text, html });
-  },
+  sendEmailRecoveryPassword,
 
-  async sendEmailVerification({
-    to,
-    subject = 'Circles of Angels: Account verification',
-    text,
-    bodyContent
-  }) {
-    logger.info('[MailService] :: Sending verification mail to:', to);
-    validateRequiredParams({
-      method: 'sendEmailVerification',
-      params: { to, subject, bodyContent }
-    });
+  sendEmailInitialRecoveryPassword,
 
-    const html = await templateParser.completeTemplate(
-      {
-        ...bodyContent,
-        frontendUrl: config.frontendUrl
-      },
-      templateNames.EMAIL_CONFIRMATION
-    );
-    await this.sendMail({ to, subject, text, html });
-  },
+  sendLowBalanceGSNAccountEmail,
 
-  async sendEmailRecoveryPassword({
-    to,
-    subject = 'Circles of Angels: Recovery Password',
-    text,
-    bodyContent
-  }) {
-    logger.info('[MailService] :: Sending recovery password mail to:', to);
-    validateRequiredParams({
-      method: 'sendEmailRecoveryPassword',
-      params: { to, subject, bodyContent }
-    });
-    const baseUrl = bodyContent.projectId
-      ? `${FRONTEND_URL}/${bodyContent.projectId}`
-      : `${FRONTEND_URL}/u`;
-    const html = this.getHTMLFromMJML({
-      mjmlFileName: templateNames.RECOVERY_PASSWORD,
-      objectData: {
-        ...bodyContent,
-        ...languages.resetPasswordEmail,
-        frontendUrl: baseUrl,
-        URL_LOGO,
-        URL_LOCKED_WINDOW
-      }
-    });
-    await this.sendMail({ to, subject, text, html });
-  },
+  sendInitialUserResetPassword,
 
-  async sendEmailInitialRecoveryPassword({
-    to,
-    subject = 'Circles of Angels: Reset Password',
-    text,
-    bodyContent
-  }) {
-    logger.info('[MailService] :: Sending recovery password mail to:', to);
-    validateRequiredParams({
-      method: 'sendEmailRecoveryPassword',
-      params: { to, subject, bodyContent }
-    });
+  sendPublishProject,
 
-    const html = this.getHTMLFromMJML({
-      mjmlFileName: templateNames.WELCOME,
-      objectData: {
-        ...bodyContent,
-        frontendUrl: FRONTEND_URL,
-        URL_LOGO,
-        URL_LOCKED_WINDOW,
-        ...languages.welcomeEmail
-      }
-    });
-    await this.sendMail({ to, subject, text, html });
-  },
+  sendEmailCloneInReview,
 
-  async sendLowBalanceGSNAccountEmail(to, account, balance) {
-    logger.info(
-      `[MailService] :: Sending low balance in Main account ${account} to ${to}`
-    );
-    validateRequiredParams({
-      method: 'sendLowBalanceGSNAccountEmail',
-      params: { to, account, balance }
-    });
-
-    const html = await templateParser.completeTemplate(
-      {
-        title: 'Alert COA main account is running out of balance.',
-        bodyText: `This is a reminder that the COA Gas station is running out of money, having currently ${balance} WEIs left. 
-Please add more to avoid transactions not being able to execute. 
-Remember the address to transfer the money to is: ${account}`
-      },
-      templateNames.ALERT
-    );
-    const subject = 'COA Gas Station Balance Alert';
-    await this.sendMail({ to, subject, html });
-  },
-
-  async sendInitialUserResetPassword({
-    to,
-    subject = 'Circles of Angels: Reset Password',
-    text,
-    bodyContent
-  }) {
-    logger.info('[MailService] :: Sending recovery password mail to:', to);
-    validateRequiredParams({
-      method: 'sendInitialUserResetPassword',
-      params: { to, subject, bodyContent }
-    });
-    const baseUrl = bodyContent.projectId
-      ? `${FRONTEND_URL}/${bodyContent.projectId}`
-      : `${FRONTEND_URL}/u`;
-
-    const html = this.getHTMLFromMJML({
-      mjmlFileName: templateNames.WELCOME_USER,
-      objectData: {
-        ...bodyContent,
-        ...languages.welcomeUserEmail,
-        frontendUrl: baseUrl,
-        URL_LOGO,
-        URL_LOCKED_WINDOW
-      }
-    });
-    await this.sendMail({ to, subject, text, html });
-  },
-
-  async sendPublishProject({
-    to,
-    subject = 'Circles of Angels: Project published',
-    text,
-    bodyContent
-  }) {
-    validateRequiredParams({
-      method: 'sendPublishProject',
-      params: { to, subject, bodyContent }
-    });
-    const html = this.getHTMLFromMJML({
-      mjmlFileName: templateNames.PUBLISH_PROJECT,
-      objectData: {
-        ...bodyContent,
-        ...languages.publishedProjectEmail,
-        frontendUrl: FRONTEND_URL,
-        URL_LOGO,
-        URL_UPLOAD_TO_CLOUD
-      }
-    });
-    await this.sendMail({ to, subject, text, html });
-  },
-  getHTMLFromMJML({ mjmlFileName, objectData }) {
-    const mjmlFileContent = fs.readFileSync(
-      `${TEMPLATES_DIRECTORY_PATH}/${mjmlFileName}.mjml`
-    );
-    let htmlOutput = mjml2html(mjmlFileContent.toString()).html;
-    Object.keys(objectData).forEach(key => {
-      const regExp = new RegExp(`{{${key}}}`, 'g');
-      htmlOutput = htmlOutput.replace(regExp, objectData[key]);
-    });
-    return htmlOutput;
-  },
-  async sendEmailCloneInReview({
-    to,
-    subject = 'Circles of Angels: Project In Review',
-    text,
-    bodyContent
-  }) {
-    validateRequiredParams({
-      method: 'sendEmailProjectInReview',
-      params: { to, subject, bodyContent }
-    });
-    const html = this.getHTMLFromMJML({
-      mjmlFileName: templateNames.CLONE_IN_REVIEW,
-      objectData: {
-        ...bodyContent,
-        ...languages.cloneInReviewEmail,
-        frontendUrl: FRONTEND_URL,
-        URL_LOGO,
-        URL_UPLOAD_TO_CLOUD
-      }
-    });
-    await this.sendMail({ to, subject, text, html });
-  },
-
-  async sendEmails({ project, action, users }) {
-    try {
-      logger.info('[ProjectService] :: About to send project action emails');
-      const sendEmailMethodName = SEND_EMAIL_BY_ACTION[action];
-      const usersToSendEmail =
-        users ||
-        (await this.projectService.getUsersByProjectId({
-          projectId: project.id
-        }));
-      const { projectName, id } = project;
-      const bodyContent = {
-        projectName,
-        projectId: id
-      };
-      await Promise.all(
-        usersToSendEmail.map(({ email }) =>
-          this[sendEmailMethodName]({
-            to: email,
-            bodyContent
-          })
-        )
-      );
-    } catch (error) {
-      logger.error(
-        '[ProjectService] :: There was an error trying to send project action emails',
-        error
-      );
-      throw new COAError(errors.mail.EmailNotSent);
-    }
-  }
+  sendEmails
 };
